b7dec9338e953dc88dcf961eec32123bfc674068
Eric-Noblot <eric.noblot@gmail.com>
2024-01-31 11:02:14 +0100

    Gestion des routes

---
+++ routes/trips.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const Trip = require('../models/trips');
+const moment = require("moment")
+
+
+router.get('/', (req, res) => {
+	Trip.find().then(data => {
+		res.json({  trips: data });
+	});
+});
+
+
+router.post("/", (req, res) => {
+	const dayToFind = moment(req.body.date);
+	
+	Trip.find({
+		departure: req.body.departure,
+		arrival: req.body.arrival,
+		date : {
+			$gte: dayToFind.toDate(),
+			$lte: moment(dayToFind).endOf("day").toDate()
+		}
+	}).then((data) => {
+		console.log(data)
+		if (data.length === 0) {
+			res.json({result: false, error :"No trips founded"})
+		} else {
+			res.json({result: true, allTrips : data})
+		}
+	})
+})
+
+  
+module.exports = router;
